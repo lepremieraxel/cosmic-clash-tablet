2c78f04fc78db7b83e60ba2487a3b2d6a86f2340
FanNevada <alexandre.martins.mmi@gmail.com>
2023-09-20 14:13:26 +0200

    GridShip qui marche

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -4,23 +4,79 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    public bool iCanIronAndGun;
     Grid myGrid;
+    private void Update()
+    {
+        if (this.gameObject.tag == "IronCube")
+        {
+
+        }
+    }
+
     private void Start()
     {
         myGrid = GameObject.Find("Grid").GetComponent<Grid>();
     }
     private void OnMouseOver()
     {
-        this.GetComponent<Renderer>().material.color = Color.yellow;
+        if (myGrid.newRessourcePrefab.name == "IronCube" || myGrid.newRessourcePrefab.name == "GunCube")
+        {
+            if(iCanIronAndGun == true)
+            {
+                this.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+            else
+            {
+                this.GetComponent<Renderer>().material.color = Color.red;
+            }
+            
+        }
+        else
+        {
+            this.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+
     }
     private void OnMouseExit()
     {
         this.GetComponent<Renderer>().material.color = Color.white;
     }
+
     private void OnMouseUp()
     {
-        GameObject go = Instantiate(myGrid.ironPrefab, this.transform.position, Quaternion.identity);
+        if (myGrid.newRessourcePrefab.name == "IronCube" || myGrid.newRessourcePrefab.name == "GunCube" )
+        {
+            if (iCanIronAndGun == true)
+            {
+                PlaceRessource();
+            }
+            
+        }
+        else
+        {
+            PlaceRessource();
+        }
+        
+    }
+    void PlaceRessource()
+    {
+        GameObject go = Instantiate(myGrid.newRessourcePrefab, this.transform.position, Quaternion.identity);
+        go.transform.SetParent(this.transform);
+        this.gameObject.tag = myGrid.newRessourcePrefab.name;
         go.name = "Ressources" + gameObject.name;
     }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("IronCube")|| other.CompareTag("CoreCube") || other.CompareTag("GunCube"))
+        {
+            iCanIronAndGun = true;
+        }      
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        iCanIronAndGun = false;
+    }
 }
